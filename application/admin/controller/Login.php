8c11d4bc697b89c847e96d1b3f12dff9d1654aa1
dyc <email@example.com>
2019-06-20 22:09:44 +0800

    dyc 5.2

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -11,7 +11,14 @@ class Login extends \think\Controller
 
     public function login()
     {
-
+        $name=input("get.name1");
+//        var_dump($uname);die;
+        $password=input("get.password1");
+        $sql="select * from admin where aname='$name' and apassword='$password'";
+        $arr=Db::query($sql);
+        if(!empty($arr)){
+            echo "123";
+        }
     }
 
     public function hello($name = 'ThinkPHP5')

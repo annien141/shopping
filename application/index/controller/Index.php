bd972da59f9f7e215d2fbbbb3a4e0cc1e9051081
dyc <email@example.com>
2019-06-20 09:38:02 +0800

    dyc 4

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,11 +1,15 @@
 <?php
 namespace app\index\controller;
+use Db;
 
 class Index
 {
     public function index()
     {
+        $sql="select * from user";
+        $arr=Db::query($sql);
         echo 123;
+        var_dump($arr);
     }
 
     public function hello($name = 'ThinkPHP5')

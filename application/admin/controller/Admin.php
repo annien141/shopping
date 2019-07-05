07658e5c3aee29290119745436d517ff49a79bd0
dyc <email@example.com>
2019-07-05 17:15:26 +0800

    dyc 10.3

---
+++ application/admin/controller/Admin.php
@@ -0,0 +1,210 @@
+<?php
+namespace app\admin\controller;
+use think\Db;
+use think\db\Query;
+use think\Session;
+use think\Controller;
+use gmars\rbac\Rbac;
+use Request;
+use app\admin\model\Brand as BrandModel;
+if (!session_id()) session_start();
+class Admin extends Common
+{
+
+
+    public function admin_list()
+    {
+        return $this->fetch("admin_list");
+    }
+
+//    public function admin()
+//    {
+//        $id=input('post.id');
+//        $arr=Db::table('role')->where('id',$id)->select();
+//        $res=['code'=>'0','status'=>'ok','data'=>$arr];
+//        echo json_encode($res);
+//    }
+    public function show()
+    {
+        $sql = "select u.id ,u.user_name,u.mobile,u.last_login_time,ur.user_id ,ur.role_id ,r.id as rid,r.name as rname,r.description from user as u join user_role as ur on ur.user_id=u.id join role as r on r.id=ur.role_id order by u.id";
+        $arr = Db::query($sql);
+        echo json_encode($arr);
+    }
+    
+    public function show1(){
+        $sql = "select * from role";
+        $arr = Db::query($sql);
+        echo json_encode($arr);
+    }
+
+    public function add()
+    {
+        return $this->fetch("addadmin");
+    }
+
+    public function addAction()
+    {
+        $data = input();
+        $name = $data['name1'];
+        $phone = $data['phone'];
+        $time=$data['time'];
+        $select=$data['select'];
+        $password=md5($data['password']);
+        $time=str_replace('T',' ',$time);
+        $validate = new \app\admin\validate\Permission;
+        if (!$validate->check($data)) {
+            $arr = ['code' => '4', 'status' => 'error', 'data' => $validate->getError()];
+            echo json_encode($arr);
+            die;
+        }
+
+        $str = $phone;
+        $isMatched = preg_match('/^1[3-8]{1}[0-9]{9}$/', $str, $matches);
+        if($isMatched==0){
+            $arr = ['code' => '5', 'status' => 'error', 'data' => "手机号格式错误"];
+            echo json_encode($arr);
+            die;
+        }
+
+        if (empty($name)) {
+            $arr = ['code' => '4', 'status' => 'error', 'data' => "用户名不能为空"];
+            echo json_encode($arr);
+            die;
+        }
+        if (empty($password)) {
+            $arr = ['code' => '3', 'status' => 'error', 'data' => "密码不能为空"];
+            echo json_encode($arr);
+            die;
+        }
+        if (empty($phone)) {
+            $arr = ['code' => '1', 'status' => 'error', 'data' => "手机号不能为空"];
+            echo json_encode($arr);
+            die;
+        }
+        if (empty($time)) {
+            $arr = ['code' => '2', 'status' => 'error', 'data' => "登录时间不能为空"];
+            echo json_encode($arr);
+            die;
+        }
+
+        $sql="select * from user where user_name='$name' or mobile='$phone'";
+        $getarr =Db::query($sql);
+
+        if (empty($getarr)) {
+            $sql="insert into user(`user_name`,`mobile`,`last_login_time`,`password`)values('$name','$phone','$time','$password')";
+            $arr1 =Db::query($sql);
+
+            $sql1="select * from user where user_name='$name'";
+            $arr2 =Db::query($sql1);
+            $uid=$arr2[0]['id'];
+
+            $sql2="insert into user_role(`user_id`,`role_id`)values('$uid','$select')";
+            $arr3 =Db::query($sql2);
+
+            $arr = ['code' => '0', 'status' => 'ok', 'data' => "添加成功"];
+        } else {
+            $arr = ['code' => '6', 'status' => 'error', 'data' => "用户名已存在"];
+        }
+        echo json_encode($arr);
+    }
+
+    function deleteMore(){
+        $id = input("post.id");
+        $data= input();
+        $validate = new \app\admin\validate\Delete;
+        if (!$validate->check($data)) {
+            $arr = ['code' => '4', 'status' => 'error', 'data' => $validate->getError()];
+            echo json_encode($arr);
+            die;
+        }
+        $arr=explode(",",$id);
+        array_shift($arr);
+        $id1=implode($arr," or id=");
+        $id2=implode($arr," or user_id=");
+        $arr3 =Db::query("delete from user where id=$id1");
+
+        $arr4 =Db::query("delete from user_role where user_id=$id2");
+        $arr1 = ['code' => '0', 'status' => 'ok', 'data' => "删除成功"];
+        echo json_encode($arr1);
+    }
+    function delete(){
+        $id = input("post.id");
+        $data = input();
+        $validate = new \app\admin\validate\Delete();
+        if (!$validate->check($data)) {
+            $arr = ['code' => '4', 'status' => 'error', 'data' => $validate->getError()];
+            echo json_encode($arr);
+            die;
+        }
+
+        $arr1 =Db::query("delete from user where id=$id");
+        $arr2 =Db::query("delete from user_role where user_id=$id");
+
+        $arr = ['code' => '0', 'status' => 'ok', 'data' => "删除成功"];
+        echo json_encode($arr);
+    }
+
+    function pu_update(){
+        $data= input();
+        $id = $data['id'];
+        $name = $data['name'];
+        $phone = $data['phone'];
+        $time= $data['time'];
+        $time=str_replace('T',' ',$time);
+        $select2= $data['select2'];
+        $validate = new \app\admin\validate\Permission;
+        if (!$validate->check($data)) {
+            $arr = ['code' => '4', 'status' => 'error', 'data' => $validate->getError()];
+            echo json_encode($arr);
+            die;
+        }
+        if (empty($name)) {
+            $arr = ['code' => '4', 'status' => 'error', 'data' => "用户名不能为空"];
+            echo json_encode($arr);
+            die;
+        }
+        if (empty($time)) {
+            $arr = ['code' => '2', 'status' => 'error', 'data' => "登录时间不能为空"];
+            echo json_encode($arr);
+            die;
+        }
+        if (empty($phone)) {
+            $arr = ['code' => '1', 'status' => 'error', 'data' => "手机号不能为空"];
+            echo json_encode($arr);
+            die;
+        }
+        $str = $phone;
+        $isMatched = preg_match('/^1[3-8]{1}[0-9]{9}$/', $str, $matches);
+        if($isMatched==0){
+            $arr = ['code' => '5', 'status' => 'error', 'data' => "手机号格式错误"];
+            echo json_encode($arr);
+            die;
+        }
+
+        $sql="select * from user where user_name='$name' or mobile='$phone'";
+        $arr2=Db::query($sql);
+        if (empty($arr2)) {
+            $arr=Db::query("update user set user_name='$name',mobile='$phone',last_login_time='$time' where id=$id");
+
+            $arr=Db::query("update user_role set role_id='$select2' where user_id=$id");
+            
+            $arr5 = ['code' => '0', 'status' => 'ok', 'data' => "修改成功"];
+            echo json_encode($arr5);
+        }else{
+            foreach ($arr2 as $key =>$value){
+                if($value['id']!=$id) {
+                    $arr3 = ['code' => '7', 'status' => 'error', 'data' => "用户名或手机号已存在"];
+                    echo json_encode($arr3);
+                    die;
+                }
+            }
+            $arr=Db::query("update user set user_name='$name',mobile='$phone',last_login_time='$time' where id=$id");
+
+            $arr=Db::query("update user_role set role_id='$select2' where user_id=$id");
+
+            $arr3 = ['code' => '0', 'status' => 'ok', 'data' => "修改成功"];
+            echo json_encode($arr3);
+
+        }
+    }
+}
--- runtime/temp/852c03198081162d0aba0c4e2f102118.php
+++ runtime/temp/852c03198081162d0aba0c4e2f102118.php
@@ -1,4 +1,4 @@
-<?php /*a:3:{s:79:"D:\01\phpstudy\PHPTutorial\WWW\shopping\application\admin\view\index\index.html";i:1561109067;s:74:"D:\01\phpstudy\PHPTutorial\WWW\shopping\application\admin\view\header.html";i:1561978254;s:74:"D:\01\phpstudy\PHPTutorial\WWW\shopping\application\admin\view\footer.html";i:1561108986;}*/ ?>
+<?php /*a:3:{s:79:"D:\01\phpstudy\PHPTutorial\WWW\shopping\application\admin\view\index\index.html";i:1561109067;s:74:"D:\01\phpstudy\PHPTutorial\WWW\shopping\application\admin\view\header.html";i:1562290958;s:74:"D:\01\phpstudy\PHPTutorial\WWW\shopping\application\admin\view\footer.html";i:1561108986;}*/ ?>
 
 <!--_meta 作为公共模版分离出去-->
 <!DOCTYPE HTML>
@@ -136,7 +136,7 @@
                 <ul>
                     <li><a href="<?php echo url('permission/admin_role'); ?>" title="角色管理">角色管理</a></li>
                     <li><a href="<?php echo url('permission/admin_permission'); ?>" title="权限管理">权限管理</a></li>
-                    <li><a href="<?php echo url('permission/admin_list'); ?>" title="管理员列表">管理员列表</a></li>
+                    <li><a href="<?php echo url('admin/admin_list'); ?>" title="管理员列表">管理员列表</a></li>
                     <li><a href="<?php echo url('permission/list1'); ?>" title="品牌管理">权限分类管理</a></li>
                 </ul>
             </dd>
